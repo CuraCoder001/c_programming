a1181a18675feca8d5e0af2a433ed32c61dc2b16
CuraCoder001 <adetejumaryam@gmail.com>
2025-01-20 18:47:57 +0100

    lesson2

---
+++ lesson2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	x = 10;
+
+	//Expression - Anything that can be evaluated (to 1 value)
+	//Statement
+	//declaration
+	//initialization
+	
+	int y = x / 2;
+
+	printf("%d\n",y);
+	return 0;
+}
+
+//space insensitive language.
